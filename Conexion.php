49a9539982c200a54dd5e7b64fb122ce8ca95e65
erick cantillo <erick.cantillo97@gmail.com>
2025-07-25 12:51:33 -0500

    corregido error de consulta

--- Conexion.php
+++ Conexion.php
@@ -9,16 +9,17 @@ class Conexion
 
     public function conectar()
     {
-        $conn = new PDO("mysql:host=localhost;dbname=pruebamega", "root", "");
+        $conn = new PDO("mysql:host=localhost", "root", "");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql_create_db = "CREATE DATABASE IF NOT EXISTS " . self::DB;
+        $conn->exec($sql_create_db);
         $conn_db = new PDO("mysql:host=localhost;dbname=" . self::DB, self::USER, self::PASS);
         $conn->exec($sql_create_db);
         $conn_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         
         $sql_create_table = "
-            CREATE TABLE IF NOT EXISTS " . self::DB . " (
+            CREATE TABLE IF NOT EXISTS persons (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 name VARCHAR(255) NOT NULL,
                 avatar VARCHAR(255) NOT NULL,
